28ab3df4837a383123a9d1bdb67a46f4049ef35f
Diego Borges <diegorp2013@hotmail.com>
2018-11-11 18:26:05 -0200

    Trabalho Marcelo

---
+++ README.md
@@ -0,0 +1,5 @@
+﻿-Trabalho de Programação Orientada a Objeto
+-Nome: Diego Borges RA: D38FFD9
+-Nome: Wesley de Lima Ribeiro RA: 
+D4458G0
+-Profº : Marcelo gomes
---
+++ trabalhoMarcelo/trabalhoMarcelo/PessoaAgenda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalho
+{
+    public class PessoaAgenda
+    {
+        public string Nome { get; set; }
+        public string Numero { get; set; }
+        public string Celular { get; set; }
+        public string Email { get; set; }
+    }
+}
